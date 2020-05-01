e2bfe1d2dc783c53aac851505861e3a4eb900bf7
Dmitry Korotin <dev.korotin@gmail.com>
2020-05-01 23:08:39 +0400

    brown/week3/lazy_eval

---
+++ solutions/brown/week3/lazy_eval/main.cpp
@@ -0,0 +1,57 @@
+#include "test_runner.h"
+
+#include <functional>
+#include <string>
+
+using namespace std;
+
+template<typename T>
+class LazyValue {
+public:
+    explicit LazyValue(function<T()> init) : init(move(init)) {}
+
+    bool HasValue() const {
+        return static_cast<bool>(cache);
+    }
+
+    const T &Get() const {
+        if (!cache) {
+            cache = init();
+        }
+        return *cache;
+    }
+
+private:
+    mutable optional<T> cache;
+    std::function<T()> init;
+};
+
+void UseExample() {
+    const string big_string = "Giant amounts of memory";
+
+    LazyValue<string> lazy_string([&big_string] { return big_string; });
+
+    ASSERT(!lazy_string.HasValue());
+    ASSERT_EQUAL(lazy_string.Get(), big_string);
+    ASSERT(lazy_string.HasValue());
+    ASSERT_EQUAL(lazy_string.Get(), big_string);
+}
+
+void TestInitializerIsntCalled() {
+    bool called = false;
+
+    LazyValue<int> lazy_int([&called] {
+        called = true;
+        return 0;
+    });
+    ASSERT(!called);
+    lazy_int.Get();
+    ASSERT(called);
+}
+
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr, UseExample);
+    RUN_TEST(tr, TestInitializerIsntCalled);
+    return 0;
+}
