89dfb81ff0c889f725b88f78fece4139b9b16b84
Dmitry Korotin <dev.korotin@gmail.com>
2020-05-02 22:36:37 +0400

    add brown/week3/object-pool

---
+++ solutions/brown/week3/object-pool/main.cpp
@@ -0,0 +1,80 @@
+#include "test_runner.h"
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <list>
+#include <stdexcept>
+#include <set>
+
+using namespace std;
+
+template<class T>
+class ObjectPool {
+public:
+    T *Allocate() {
+        if (pool.empty()) {
+            pool.emplace_back(new T);
+        }
+        auto [iter, ok] = allocated.insert(pool.front());
+        pool.pop_front();
+        return *iter;
+    }
+
+    T *TryAllocate() {
+        if (pool.empty()) {
+            return nullptr;
+        }
+        return Allocate();
+    }
+
+    void Deallocate(T *object) {
+        if (allocated.find(object) == allocated.end()) {
+            throw invalid_argument("");
+        }
+        allocated.erase(object);
+        pool.push_back(object);
+    }
+
+    ~ObjectPool() {
+        for (auto x : pool) {
+            delete x;
+        }
+
+        for (auto x : allocated) {
+            delete x;
+        }
+    }
+
+private:
+    list<T*> pool;
+    set<T*> allocated;
+};
+
+void TestObjectPool() {
+    ObjectPool<string> pool;
+
+    auto p1 = pool.Allocate();
+    auto p2 = pool.Allocate();
+    auto p3 = pool.Allocate();
+
+    *p1 = "first";
+    *p2 = "second";
+    *p3 = "third";
+
+    pool.Deallocate(p2);
+    ASSERT_EQUAL(*pool.Allocate(), "second");
+
+    pool.Deallocate(p3);
+    pool.Deallocate(p1);
+    ASSERT_EQUAL(*pool.Allocate(), "third");
+    ASSERT_EQUAL(*pool.Allocate(), "first");
+
+    pool.Deallocate(p1);
+}
+
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr, TestObjectPool);
+    return 0;
+}
