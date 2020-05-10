983b766a77a835e2ad0c8dfed384c27a2b8eafb2
Dmitry Korotin <dev.korotin@gmail.com>
2020-05-10 13:30:31 +0400

    pass tests

--- Solution.cpp
+++ Solution.cpp
@@ -7,20 +7,17 @@ using namespace std;
 class OrdinaryShape : public IShape {
 public:
   void SetPosition(Point p) final { this->point = p; }
+
   Point GetPosition() const final { return this->point; }
 
   void SetSize(Size s) final { this->size = s; }
+
   Size GetSize() const final { return this->size; }
 
   void SetTexture(std::shared_ptr<ITexture> t) final { this->texture = t; }
-  ITexture* GetTexture() const final { return this->texture.get(); }
-protected:
-  shared_ptr<ITexture> texture;
-  Point point;
-  Size size;
-};
 
-class RectangleShape : public OrdinaryShape {
+  ITexture* GetTexture() const final { return this->texture.get(); }
+
   bool IsPointInTexture(Point p) const {
     if (!texture) {
       return false;
@@ -29,6 +26,14 @@ class RectangleShape : public OrdinaryShape {
     auto textureSize = texture->GetSize();
     return p.y < textureSize.height && p.x < textureSize.width;
   }
+
+protected:
+  shared_ptr<ITexture> texture;
+  Point point;
+  Size size;
+};
+
+class RectangleShape : public OrdinaryShape {
 public:
   std::unique_ptr<IShape> Clone() const final {
     return make_unique<RectangleShape>(*this);
@@ -57,7 +62,16 @@ public:
   }
 
   void Draw(Image& image) const final {
-
+    int maxHeight = min(this->point.y + this->size.height, static_cast<int>(image.size()));
+    int maxWidth = min(this->point.x + this->size.width, static_cast<int>((image.empty() ? 0 : image[0].size())));
+    for (int y = this->point.y; y < maxHeight; ++y) {
+      for (int x = this->point.x; x < maxWidth; ++x) {
+        Point texturePoint{x - this->point.x, y - this->point.y};
+        if (IsPointInEllipse(texturePoint, this->size) && this->IsPointInTexture(texturePoint)) {
+          image[y][x] = texture->GetImage()[texturePoint.y][texturePoint.x];
+        }
+      }
+    }
   }
 };
 
