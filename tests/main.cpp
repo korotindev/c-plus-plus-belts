8c2a5addca4bd02f9af5c749cd4f9ef47d4ed17c
Dmitry Korotin <dev.korotin@gmail.com>
2020-10-02 10:46:40 +0000

    add files

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
+#include "tests.h"
+#include "utils/test_runner.h"
+
+#include <functional>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
-int main() {
-  cout << "Tests runner" << endl;
+const static unordered_map<string, function<void()>> tests = {
+    {"1", TestIntegrationTest1},
+    {"2", TestIntegrationTest2},
+    {"3", TestIntegrationTest3},
+    {"4", TestIntegrationTest4},
+};
 
+int main(int argc, char *argv[]) {
+  TestRunner tr;
+
+  if (argc == 1) {
+    for (auto &[name, test] : tests) {
+        tr.RunTest(test, "test " + name);
+    }
+  } else {
+    if (auto it = tests.find(argv[1]); it != tests.end()) {
+      tr.RunTest(it->second, "test " + it->first);
+    } else {
+      cerr << "Test not found";
+      return 1;
+    }
+  }
   return 0;
 }
