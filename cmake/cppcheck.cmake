4e914555bcb65971c5c3e32ac40020043ddf3575
Dmitry Korotin <dev.korotin@gmail.com>
2020-10-07 14:47:28 +0000

    update env

--- cmake/cppcheck.cmake
+++
@@ -1,8 +0,0 @@
-find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
-if (CMAKE_CXX_CPPCHECK)
-    list(
-        APPEND CMAKE_CXX_CPPCHECK 
-            "--suppress=style"
-            "--std=c++20"
-    )
-endif()
