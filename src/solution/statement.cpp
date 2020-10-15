a37a558cf5f04a169548efcde2b8f97facb99939
Dmitry Korotin <dev.korotin@gmail.com>
2020-10-15 16:40:45 +0000

    parse simple program

--- src/solution/statement.cpp
+++ src/solution/statement.cpp
@@ -14,6 +14,7 @@ namespace Ast {
   ObjectHolder Assignment::Execute(Closure& closure) {
     // TODO think about closure passing
     closure[var_name] = right_value->Execute(closure);
+    return ObjectHolder::Share(*closure[var_name]);
   }
 
   Assignment::Assignment(std::string var, std::unique_ptr<Statement> rv) : var_name(move(var)), right_value(move(rv)) {}
@@ -23,17 +24,15 @@ namespace Ast {
   VariableValue::VariableValue(std::vector<std::string> dotted_ids_) : dotted_ids(move(dotted_ids_)) {}
 
   ObjectHolder VariableValue::Execute(Closure& closure) {
-    // TODO: check for overall adequacy 
+    // TODO: check for overall adequacy
     ObjectHolder oh = closure[dotted_ids[0]];
     for (size_t i = 1; i < dotted_ids.size(); i++) {
+      // TODO: Check object for nullptr
       Runtime::ClassInstance* object = oh.TryAs<Runtime::ClassInstance>();
-      // TODO: Check for nullptr ?
-      auto fields_closure = object->Fields();
       const auto& id = dotted_ids[i];
-      if (fields_closure.contains(id)) {
-        oh = fields_closure.at(id);
+      if (auto it = object->Fields().find(id); it != object->Fields().end()) {
+        oh = it->second;
       } else {
-        // WRONG
         oh = ObjectHolder::Share(*object);
         break;
       }
@@ -42,26 +41,59 @@ namespace Ast {
     return oh;
   }
 
-  unique_ptr<Print> Print::Variable(std::string var) {}
+  unique_ptr<Print> Print::Variable(std::string var) {
+    return make_unique<Print>(make_unique<Ast::VariableValue>(var));
+  }
 
-  Print::Print(unique_ptr<Statement> argument) {}
+  Print::Print(unique_ptr<Statement> argument_) { args.emplace_back(move(argument_)); }
 
-  Print::Print(vector<unique_ptr<Statement>> args) {}
+  Print::Print(vector<unique_ptr<Statement>> args_) : args(move(args_)) {}
 
-  ObjectHolder Print::Execute(Closure& closure) {}
+  ObjectHolder Print::Execute(Closure& closure) {
+    for (auto& arg : args) {
+      ObjectHolder oh = arg->Execute(closure);
+      if (oh) {
+        oh->Print(*output);
+      } else {
+        *output << "None";
+      }
+      *output << " ";
+    }
+    return ObjectHolder::None();
+  }
 
   ostream* Print::output = &cout;
 
   void Print::SetOutputStream(ostream& output_stream) { output = &output_stream; }
 
-  MethodCall::MethodCall(std::unique_ptr<Statement> object, std::string method,
-                         std::vector<std::unique_ptr<Statement>> args) {}
+  MethodCall::MethodCall(std::unique_ptr<Statement> object_, std::string method_,
+                         std::vector<std::unique_ptr<Statement>> args_)
+      : object(move(object_)), method(move(method_)), args(move(args_)) {}
 
-  ObjectHolder MethodCall::Execute(Closure& closure) {}
+  ObjectHolder MethodCall::Execute(Closure& closure) {
+    // TODO: check obj for nullptr
+    auto obj = object->Execute(closure).TryAs<Runtime::ClassInstance>();
+    vector<Runtime::ObjectHolder> computed_args;
+    transform(args.begin(), args.end(), back_inserter(computed_args),
+              [&closure](unique_ptr<Statement>& arg) { return arg->Execute(closure); });
+
+    return obj->Call(method, computed_args);
+  }
 
   ObjectHolder Stringify::Execute(Closure& closure) {}
 
-  ObjectHolder Add::Execute(Closure& closure) {}
+  ObjectHolder Add::Execute(Closure& closure) {
+    auto lhs_oh = lhs->Execute(closure);
+    auto rhs_oh = rhs->Execute(closure);
+    if (auto lhs_ptr = lhs_oh.TryAs<Runtime::Number>()) {
+      auto rhs_ptr = rhs_oh.TryAs<Runtime::Number>();
+      return ObjectHolder::Own(Runtime::Number(lhs_ptr->GetValue() + rhs_ptr->GetValue()));
+    } else {
+      auto lhs_ptr_str = lhs_oh.TryAs<Runtime::String>();
+      auto rhs_ptr_str = rhs_oh.TryAs<Runtime::String>();
+      return ObjectHolder::Own(Runtime::String(lhs_ptr_str->GetValue() + rhs_ptr_str->GetValue()));
+    }
+  }
 
   ObjectHolder Sub::Execute(Closure& closure) {}
 
@@ -69,16 +101,27 @@ namespace Ast {
 
   ObjectHolder Div::Execute(Runtime::Closure& closure) {}
 
-  ObjectHolder Compound::Execute(Closure& closure) {}
+  ObjectHolder Compound::Execute(Closure& closure) {
+    for (auto& statement : statements) {
+      auto result_holder = statement->Execute(closure);
+      if (result_holder) {
+        if (typeid(statement.get()) == typeid(Ast::Return*)) {
+          return result_holder;
+        }
+      }
+    }
 
-  ObjectHolder Return::Execute(Closure& closure) {}
+    return ObjectHolder::None();
+  }
+
+  ObjectHolder Return::Execute(Closure& closure) { return statement->Execute(closure); }
 
   ClassDefinition::ClassDefinition(ObjectHolder class_) {}
 
   ObjectHolder ClassDefinition::Execute(Runtime::Closure& closure) {}
 
-  FieldAssignment::FieldAssignment(VariableValue object, std::string field_name, std::unique_ptr<Statement> rv)
-      : object(std::move(object)), field_name(std::move(field_name)), right_value(std::move(rv)) {}
+  FieldAssignment::FieldAssignment(VariableValue object_, std::string field_name_, std::unique_ptr<Statement> rv_)
+      : object(std::move(object_)), field_name(std::move(field_name_)), right_value(std::move(rv_)) {}
 
   ObjectHolder FieldAssignment::Execute(Runtime::Closure& closure) {}
 
