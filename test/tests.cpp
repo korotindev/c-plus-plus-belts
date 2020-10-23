993358e47b9695591d6f6d0f04048797ffb8f26d
Dmitry Korotin <dev.korotin@gmail.com>
2020-10-23 18:59:04 +0000

    week3 start

---
+++ test/tests.cpp
@@ -0,0 +1,44 @@
+#include "tests.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "descriptions.h"
+#include "json.h"
+#include "requests.h"
+#include "sphere.h"
+#include "transport_catalog.h"
+#include "utils.h"
+#include "test_runner.h"
+
+using namespace std;
+
+void TestIntegrationGenerator(const string &testDataFolderName) {
+  auto input = ifstream("samples/" + testDataFolderName + "/input.json");
+  auto expectedOutput = ifstream("samples/" + testDataFolderName + "/expected_output.json");
+
+  const auto input_doc = Json::Load(input);
+  const auto &input_map = input_doc.GetRoot().AsMap();
+  const TransportCatalog db(Descriptions::ReadDescriptions(input_map.at("base_requests").AsArray()),
+                            input_map.at("routing_settings").AsMap(), input_map.at("render_settings").AsMap());
+  stringstream output;
+  Json::PrintValue(Requests::ProcessAll(db, input_map.at("stat_requests").AsArray()), output);
+  output << endl;
+
+  cout << output.str();
+
+  // Json::Document resultDocument = Json::Load(output);
+  // Json::Document expectedDocument = Json::Load(expectedOutput);
+  // ASSERT_EQUAL(resultDocument, expectedDocument);
+}
+
+void TestIntegrationTest1() { TestIntegrationGenerator("test1"); }
+
+void TestIntegrationTest2() { TestIntegrationGenerator("test2"); }
+
+void TestIntegrationTest3() { TestIntegrationGenerator("test3"); }
+
+void TestIntegrationTest4() { TestIntegrationGenerator("test4"); }
---
+++ test/tests.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+void TestParsing();
+void TestIntegrationGenerator(const std::string &testDataFolderName);
+void TestIntegrationTest1();
+void TestIntegrationTest2();
+void TestIntegrationTest3();
+void TestIntegrationTest4();
