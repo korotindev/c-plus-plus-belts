f5b767a7cbb95b343bf43a2a2b921bf9f1e3cba4
Dmitry Korotin <dev.korotin@gmail.com>
2020-10-29 21:37:39 +0000

    fix 4 test

--- test/integration_tests.cpp
+++ test/integration_tests.cpp
@@ -8,12 +8,12 @@
 
 #include "descriptions.h"
 #include "json.h"
+#include "map_renderer.h"
 #include "requests.h"
 #include "sphere.h"
 #include "test_runner.h"
 #include "transport_catalog.h"
 #include "utils.h"
-#include "map_renderer.h"
 
 using namespace std;
 
@@ -31,18 +31,24 @@ void FindAndPrintSvg(const Json::Document &doc, string filename) {
   }
 }
 
+template <typename ConcreteMapRenderer, class = enable_if<is_base_of<IMapRenderer, ConcreteMapRenderer>::value>::type>
+void TestMapRenderingIntegration(const Json::Document &input_doc, ostream &out) {
+  const auto &input_map = input_doc.GetRoot().AsMap();
+  const TransportCatalog db(Descriptions::ReadDescriptions(input_map.at("base_requests").AsArray()),
+                            input_map.at("routing_settings").AsMap(), input_map.at("render_settings").AsMap(),
+                            make_unique<ConcreteMapRenderer>());
+  out.precision(17);
+  Json::PrintValue(Requests::ProcessAll(db, input_map.at("stat_requests").AsArray()), out);
+  out << endl;
+}
+
 void TestIntegration(const string &testDataFolderName) {
   auto input = ifstream(testDataFolderName + "/input.json");
   auto expectedOutput = ifstream(testDataFolderName + "/expected_output.json");
 
   const auto input_doc = Json::Load(input);
-  const auto &input_map = input_doc.GetRoot().AsMap();
-  const TransportCatalog db(Descriptions::ReadDescriptions(input_map.at("base_requests").AsArray()),
-                            input_map.at("routing_settings").AsMap(), input_map.at("render_settings").AsMap(), make_unique<DefaultMapRenderer>());
   stringstream output;
-  output.precision(17);
-  Json::PrintValue(Requests::ProcessAll(db, input_map.at("stat_requests").AsArray()), output);
-  output << endl;
+  TestMapRenderingIntegration<DefaultMapRenderer>(input_doc, output);
 
   Json::Document resultDocument = Json::Load(output);
   Json::Document expectedDocument = Json::Load(expectedOutput);
