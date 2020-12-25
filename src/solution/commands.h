499be53029748d2bc8f6364e43d807f35d3a1dd6
Dmitry Korotin <korotin.dev@gmail.com>
2020-12-25 11:25:39 +0000

    prepare week6 skeleton

--- src/solution/commands.h
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-
-void ProcessRequests(std::istream &in, std::ostream &out);
-void MakeBase(std::istream &in);
--- test/test_svg.h
+++
@@ -1,6 +0,0 @@
-#include "test_runner.h"
-
-namespace TestSvg {
-  void TestRect();
-  void Run(TestRunner &tr);
-}  // namespace Svg::Test
